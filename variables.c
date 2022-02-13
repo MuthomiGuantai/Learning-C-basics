5ee8eabc8b566d60271988a80a15f46ca3a8175d
MuthomiGuantai <muthomiguantai@gmail.com>
2022-02-13 20:37:13 +0300

    creating variables and giving them value

---
+++ variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () {
+    int firstNumber = 69,secondNumber = 96;
+
+    printf("First Number: %d",firstNumber);
+
+    printf("Second Number: %d",secondNumber);
+
+    return 0;
+}
