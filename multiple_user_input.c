457b4d26b1fed816399561a73467fb33e646e8d6
MuthomiGuantai <muthomiguantai@gmail.com>
2022-02-16 00:02:37 +0300

    multiple_user_input

---
+++ multiple_user_input.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main (){
+
+    double number;
+    char letter;
+
+    printf("Input values here; ");
+    scanf("%1f %c", &number, &letter);
+
+    printf("\nWeignt = %1f", number);
+    printf("\nFirst letter of name: ");
+
+    return 0;
+}
