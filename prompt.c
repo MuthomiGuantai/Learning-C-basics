325c6aa5be05269872b4483bfd201aa6a75e76a1
MuthomiGuantai <muthomiguantai@gmail.com>
2022-02-22 13:57:41 +0300

    fgets to capture user input

---
+++ prompt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main () {
+    int age;
+    char name[15], dad_name[15];
+    double weight;
+
+    printf("Enter your age; ");
+    scanf("%d", &age);
+    printf("Enter your name; ");
+    scanf("%s", &name);
+    printf("\nEnter your father's name: ");
+    fgets(dad_name, 15, stdin);
+    printf("\nEnter your weight: ");
+    scanf("%lf", &weight);
+    printf("\nYour name is %s", name);
+    printf("Your age is %d", age);
+    printf("Your dad's name is %s", dad_name);
+    printf("Your weight is %ls", weight);
+
+}
